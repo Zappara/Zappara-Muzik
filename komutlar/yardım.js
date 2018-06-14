38550165e1b187b418167a794bb8f55e1dc8d66f
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-14 18:02:22 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,21 +1,21 @@
 const Discord = require("discord.js");
 const ayarlar = require("./ayarlar.json");
-const prefix = ayarlar.prefix;
+
 exports.run = async (bot, message, args) => {
 
 let embed = new Discord.RichEmbed()
     .setTitle("Müzik")
     .setDescription("There are currently 8 commands in this category.\n`The default prefix will remain.`")
-    .addField("`" + prefix +"çal`", "Müzik çalar")
-    .addField("`" + prefix +"neçalıyor`", "Şu anda ne çaldığını gösterir.")
-    .addField("`" + prefix +"dur`", "Çalan müziği durdurur")
-    .addField("`" + prefix +"kuyruk`", "Müzik kuyruğunu gösterir")
-    .addField("`" + prefix +"geç`", "Bir sonraki müziği çalar")
-    .addField("`" + prefix +"ses`", "Müziğin sesini ayarlar")
-    .addField("`" + prefix +"devamet`", "Durdurulan müziği devam ettirilir")
+    .addField("`" + ayarlar.prefix +"çal`", "Müzik çalar")
+    .addField("`" + ayarlar.prefix +"neçalıyor`", "Şu anda ne çaldığını gösterir.")
+    .addField("`" + ayarlar.prefix +"dur`", "Çalan müziği durdurur")
+    .addField("`" + ayarlar.prefix +"kuyruk`", "Müzik kuyruğunu gösterir")
+    .addField("`" + ayarlar.prefix +"geç`", "Bir sonraki müziği çalar")
+    .addField("`" + ayarlar.prefix +"ses`", "Müziğin sesini ayarlar")
+    .addField("`" + ayarlar.prefix +"devam`", "Durdurulan müziği devam ettirilir")
     .setImage("https://cdn.discordapp.com/attachments/440820385643233290/449932578833825816/unnamed_1.gif")
     .setThumbnail("https://cdn.discordapp.com/emojis/424321045034696724.gif")
-    .setFooter("Müzik Bot | ${prefix}davet | Sahip: Enes Onur Ata#9427 | 7/24")
+    .setFooter("Müzik Bot | ${ayarlar.prefix}davet | Sahip: Enes Onur Ata#9427 | 7/24")
 
 message.channel.send(embed);
 }
