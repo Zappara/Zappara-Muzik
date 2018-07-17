611322794b2c9acd52831024b945d164e0f8f57d
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-07-17 20:01:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -96,10 +96,7 @@ const api = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM1NDIzMTYzMDQwNTA0Mj
 module.exports = client => {
   snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`).set('Authorization', api).send({
     server_count: client.guilds.size
-  }) console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  })
   var Games = ["Müzik Bot | Türkçe Dil Desteği", "Yeni Nesil Bot | v${ayarlar.sürüm} | JavaScript ile yazılmıştır", `${ayarlar.prefix}yardım | ${client.guilds.size} sunucu | ${client.users.size} kullanıcı`];
   setInterval(function() {
     var random = Math.floor(Math.random() * (Games.length - 0 + 1) + 0);
