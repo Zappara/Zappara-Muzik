f2504690f568bebbf93a92a17e328ccb282253c7
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-07-17 19:55:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -91,11 +91,8 @@ client.on("message", message => {
 //Twitch Değişen Oynuyor
 const chalk = require('chalk');
 const moment = require('moment');
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
 const snekfetch = require('snekfetch');
 const api = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM1NDIzMTYzMDQwNTA0MjE3NiIsImJvdCI6dHJ1ZSwiaWF0IjoxNTE2Mjc0MTQ1fQ.2H9LjNjH6WFp5LmswfXAYSDsHQn2JSPPgbgf1WjSi_c';
-var prefix = ayarlar.prefix;
 module.exports = client => {
   snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`).set('Authorization', api).send({
     server_count: client.guilds.size
