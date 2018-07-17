ca7a1912249cdd3d324d22d8fd280604bfe12635
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-07-17 18:26:13 +0300

    Delete guildMemberRemove.js

--- events/guildMemberRemove.js
+++
@@ -1,5 +0,0 @@
-module.exports = member => {
-  const channel = member.guild.channels.find('name', 'g-log');
-  if (!channel) return;
-  channel.send(`**📤 |** ${member} **Sunucudan çıkış yaptı!**`);
-};
