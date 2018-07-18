74181c1ebaddce03a6e2ce999d35c2c641ee4e74
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-07-18 14:47:13 +0300

    Yardım
    Hangi komut ne işe yarar gösterir

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,25 +1,18 @@
-const Discord = require("discord.js");
-const ayarlar = require("./ayarlar.json");
+const Discord = require('discord.js');
+const ayarlar = require('./ayarlar.json');
+var prefix = ayarlar.prefix;
 
-exports.run = async (bot, message, args) => {
-
-let embed = new Discord.RichEmbed()
-    .setTitle("Müzik")
-    .setDescription("There are currently 8 commands in this category.\n`The default prefix will remain.`")
-    .addField("`" + ayarlar.prefix +"çal`", "Müzik çalar")
-    .addField("`" + ayarlar.prefix +"neçalıyor`", "Şu anda ne çaldığını gösterir.")
-    .addField("`" + ayarlar.prefix +"dur`", "Çalan müziği durdurur")
-    .addField("`" + ayarlar.prefix +"kuyruk`", "Müzik kuyruğunu gösterir")
-    .addField("`" + ayarlar.prefix +"geç`", "Bir sonraki müziği çalar")
-    .addField("`" + ayarlar.prefix +"ses`", "Müziğin sesini ayarlar")
-    .addField("`" + ayarlar.prefix +"devam`", "Durdurulan müziği devam ettirilir")
-    .setImage("https://cdn.discordapp.com/attachments/440820385643233290/449932578833825816/unnamed_1.gif")
-    .setThumbnail("https://cdn.discordapp.com/emojis/424321045034696724.gif")
-    .setFooter("Müzik Bot | ${ayarlar.prefix}davet | Sahip: Enes Onur Ata#9427 | 7/24")
-
-message.channel.send(embed);
+module.exports.run = async (bot, message, args) => {
+  let embed = new Discord.RichEmbed()
+    .setTitle("KATEGORİLER")
+    .setAuthor("Prefix: ${ayarlar.prefix}", bot.user.displayAvatarURL)
+    .addField("Yardım", "`" + prefix + "yardım`", true)
+    .addField("Müzik", "`" + prefix + "müzik`", true)
+    .addField("İstatistikler", "`" + prefix + "istatistikler`", true)
+    .addField("Geliştirici", "`" + prefix + "geliştirici`", true)
+    .setFooter("Müzik Botu" + prefix + "yardım" + "Tüm Hakları EnesOnurAta#9427 ye aittir.")
+    message.channel.send(embed);
 }
-
-exports.help = {
-  name: "yardım"
+module.exports.help = {
+    name: "yardım"
 }
