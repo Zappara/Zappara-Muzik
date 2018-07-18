5136a24dad9166ca69510c13bc020f0dcf98ea4b
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-07-18 14:51:34 +0300

    İstatistikler

---
+++ komutlar/istatistikler.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const ayarlar = require("./ayarlar.json");
+var prefix = ayarlar.prefix;
+
+module.exports.run = async (bot, message, args) => {
+
+let embed = new Discord.RichEmbed()
+.setTitle(":newspaper: İstatistikler")
+.addField("`" + prefix +"istatistik`", "Botun istatistiklerini gösterir")
+.addField("`" + prefix +"bilgilerim`", "Kullanıcının istatistiklerini gösterir")
+.addField("`" + prefix +"sunucubilgi`", "Sunucunun istatistiklerini gösterir")
+.addField("`" + prefix +"ping`", "Gecikmenizi gösterir")
+
+
+message.channel.send(embed);
+}
+
+module.exports.help = {
+  name: "istatistikler"
+}
